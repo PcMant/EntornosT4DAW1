844928c7c1a9dc90415b410cc27f42b11b94c343
Normal User <Normal User@DESKTOP-SUMQVQP>
2021-03-12 16:42:07 +0100

    Versión inicial proyecto Deposito

---
+++ Deposito/src/cuentas/test.java
@@ -0,0 +1,14 @@
+package cuentas;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main.main(null);
+		System.out.println("Esto no es coña");
+		
+		CCuenta cuenta = new CCuenta();
+		cuenta.operativa_cuenta(0);
+	}
+
+}
